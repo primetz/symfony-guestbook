99f36c8a604739c5a226270f86df208b97d66ed8
Stanislav Rodikov <stanislav.rodikov@yahoo.com>
2023-09-13 23:28:26 +0300

    Жизненный цикл объектов Doctrine

--- app/src/Controller/Admin/CommentCrudController.php
+++ app/src/Controller/Admin/CommentCrudController.php
@@ -49,8 +49,6 @@ class CommentCrudController extends AbstractCrudController
 
         if (Crud::PAGE_EDIT === $pageName) {
             yield $createdAt->setFormTypeOption('disabled', true);
-        } else {
-            yield $createdAt;
         }
     }
 
